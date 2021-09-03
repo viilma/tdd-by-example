7c778176785aa9cc0a0cb27e0ec0d7ca74e64bbe
Vilma <v.zaveckiene@gmail.com>
2021-09-03 12:49:18 +0300

    Added more tests

--- src/main/java/guru/springframework/Expression.java
+++ src/main/java/guru/springframework/Expression.java
@@ -2,4 +2,8 @@ package guru.springframework;
 
 public interface Expression {
     Money reduce(Bank bank, String to);
+
+    public Expression plus(Expression addend);
+
+    Expression times(int multiplier);
 }
--- src/main/java/guru/springframework/Money.java
+++ src/main/java/guru/springframework/Money.java
@@ -35,17 +35,18 @@ public class Money implements Expression {
                 '}';
     }
 
-    public Money times(int multiplier) {
+    @Override
+    public Expression times(int multiplier) {
         return new Money(amount * multiplier, this.currency);
     }
 
-    public Expression plus(Money addend){
+    @Override
+    public Expression plus(Expression addend){
         return new Sum(this, addend);
     }
 
     @Override
     public Money reduce(Bank bank, String to){
-//        int rate = (currency.equals("CHF") && to.equals("USD")) ? 2 : 1;
         return new Money(amount/ bank.rate(this.currency, to), to);
     }
 
